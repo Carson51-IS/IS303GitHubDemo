8fc098523419bfbc8db8b13f9336819d264f18b1
DrFunDip72 <jmaximum72@gmail.com>
2025-02-20 13:34:00 -0700

    Made a function hehe
    It works.
    
    Co-Authored-By: Carson51-IS <180603584+Carson51-IS@users.noreply.github.com>

---
+++ multiply.py
@@ -0,0 +1,9 @@
+# Justin Maxwell - 2.20.2025
+# Just multiplies 2 number brotha
+
+def multiplyTwoNumbers(iNum1, iNum2):
+    results = iNum1 * iNum2
+    return results
+
+product = multiplyTwoNumbers(2, 3)
+print(product)
